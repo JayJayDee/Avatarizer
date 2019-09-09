be62442555b38eed874396fa394c5c30abc04019
JayJayDee <jindongp@gmail.com>
2019-09-09 22:45:54 +0900

    completed resource-loader test cases. added test:coverage npm script.

--- tests/resource-loader/resource-loader-index.test.js
+++ tests/resource-loader/resource-loader-index.test.js
@@ -1,6 +1,13 @@
 import { initResourceLoader } from "../../lib/resource-loader";
 
 describe('resource-loader/index tests()', () => {
+  const groupNames = [
+    'body-shapes',
+    'clothes',
+    'face-shapes',
+    'face-components',
+    'hairs'
+  ];
 
   test('if the readResources() throws error, must be throw error', () => {
     const readResources = (path) =>
@@ -14,7 +21,7 @@ describe('resource-loader/index tests()', () => {
   test('if the validate() throws error, must be throw error', () => {
     const readResources = (path) =>
       new Promise((resolve, reject) =>
-        resolve(['a', 'b', 'c']));
+        resolve(['a', 'b']));
 
     const validate = (grp) =>
       new Promise((resolve, reject) =>
@@ -22,5 +29,26 @@ describe('resource-loader/index tests()', () => {
 
     const load = initResourceLoader({ readResources, validate });
     expect(load()).toReject();
+  });
+
+  test('if every function passes, the results must be matched', () => {
+    const readResources = (path) =>
+      new Promise((resolve, reject) =>
+        resolve(['a', 'b']));
+
+    const validate = (grp) =>
+      new Promise((resolve, reject) => resolve());
+
+      const load = initResourceLoader({ readResources, validate });
+      load({ resourcePath: 'test' })
+      .then((resp) => {
+        resp.forEach((r) => {
+          expect(groupNames.includes(r.name)).toEqual(true);
+          expect(r.contents.length).toBe(2);
+        });
+      })
+      .catch((err) => {
+        throw err;
+      });
   });
 });
